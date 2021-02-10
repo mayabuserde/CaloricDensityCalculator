93ff6ae0be0e726ef8b32687826c3f41a3d3cbd4
Maya Buser De <maya.buser@gmail.com>
2021-02-10 17:46:43 -0500

    added whole grain and drinks logic

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const render = document.querySelector('.render');
 const foodForm = document.querySelector('#foodForm');
 
 class Food {
-    constructor(foodName, calories, weight, foodType) {
+    constructor(foodName, calories, weight, foodType, isWholeGrain, drinks) {
         this.foodName = foodName,
             this.calories = calories,
             this.weight = weight,
             this.foodType = foodType,
-            this.catNum = 0
-        this.catPercent = this.calories / this.weight;
+            this.catNum = 0,
+            this.isWholeGrain = isWholeGrain,
+            this.drinks = drinks,
+            this.catPercent = this.calories / this.weight;
     }
     getCategory() {
         switch (this.foodType) {
@@ -97,15 +99,51 @@ class Food {
         }
         return this.color;
     }
+
+    checkWholeGrain() {
+        if (this.isWholeGrain === 'yes' && this.catNum > 1) {
+            this.catNum = this.catNum - 1;
+        }
+    }
+
+    checkDrinks() {
+        if (this.drinks === "soda" || this.drinks === "alcohol") {
+            this.catNum = this.catNum + 1;
+        }
+
+        else if (this.drinks === "artificial") {
+            this.catNum = 2;
+        }
+
+    }
 }
 
+foodForm.elements.foodType.addEventListener('change', function (e) {
+    if (foodForm.elements.foodType.value === 'solid') {
+        document.querySelector('#wholeGrainDiv').classList.remove('d-none');
+        document.querySelector('#drinksDiv').classList.add('d-none');
 
+    }
+    else if (foodForm.elements.foodType.value === 'soup') {
+        document.querySelector('#wholeGrainDiv').classList.add('d-none');
+        document.querySelector('#drinksDiv').classList.add('d-none');
+
+    }
+    else if (foodForm.elements.foodType.value === 'liquid') {
+        document.querySelector('#wholeGrainDiv').classList.add('d-none');
+        document.querySelector('#drinksDiv').classList.remove('d-none');
+
+    }
+})
 
 foodForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    let food1 = new Food(foodForm.elements.foodName.value, parseInt(foodForm.elements.calories.value), parseInt(foodForm.elements.weight.value), foodForm.elements.foodType.value)
-    console.log(food1);
+
+    let food1 = new Food(foodForm.elements.foodName.value, parseInt(foodForm.elements.calories.value), parseInt(foodForm.elements.weight.value), foodForm.elements.foodType.value, foodForm.elements.isWholeGrain.value, foodForm.elements.drinks.value);
+    console.log(foodForm.elements.drinks.value)
     food1.getCategory();
+    food1.checkWholeGrain();
+    food1.checkDrinks();
     food1.render();
 })
 
